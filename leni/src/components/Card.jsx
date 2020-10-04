5add62d11b85391a09ce20f00b9787f79814e25e
Maxi De Filippis <maxidefilippis@gmail.com>
2020-10-03 22:19:08 -0300

    Creando CSS module carta

--- leni/src/components/Card.jsx
+++ leni/src/components/Card.jsx
@@ -1,9 +1,10 @@
 import React, { useState } from 'react';
+import style from './Card.module.css';
 
 export default function Card (prop){
     if (prop.name){
         return (
-            <div>
+            <div className={style.carta}>
                 <h3>{prop.name}</h3>
                 <img src={prop.img} alt="pokemon"/>
             </div>
